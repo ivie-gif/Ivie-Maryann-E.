64712184f12709b4dbd90d27d58de62cc0d78a99
ivie-gif <emwiongbon_ivie@yahoo.com>
2025-01-07 11:01:29 +0100

    first commit

---
+++ src/components/themes/palettes.ts
@@ -0,0 +1,24 @@
+const palettes = {
+    primary: {
+    main: "#000000",
+    light: "#F0F5FF",
+    shade: "#80ACFF",
+    border: "#CDCED9",
+    },
+    secondary: {
+    main: "#FFCFCF",
+    light: "#F0F5FF",
+    shade: "#80ACFF",
+    border: "#CDCED9",
+    },
+    background: { 
+    default: "#000000",
+    secondary: "#FFCFCF",
+    },
+    text: {
+    primary: "#FFFFFF",
+    secondary: "#000000",
+    pink: "#FFE2E2",
+    grey: "#b3c2c1"
+    }
+}
